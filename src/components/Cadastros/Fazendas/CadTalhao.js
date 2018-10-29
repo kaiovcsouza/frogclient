86ba91f970139ccda9ad9a1931f47db615328315
Kaio Souza <kaiovcsouza@gmail.com>
2018-10-29 16:43:46 -0300

    Tela de cadastro de talhao, base finalizada.

--- src/components/Cadastros/Fazendas/CadTalhao.js
+++ src/components/Cadastros/Fazendas/CadTalhao.js
@@ -1,41 +1,69 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+// Import React Table
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class CadTalhao extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { inputList: [] };
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            nomeTalhao: '',
+            areaTalhao: ''
+        };
         this.onAddBtnClick = this.onAddBtnClick.bind(this);
-        this.onRemoveBtnClick = this.onRemoveBtnClick.bind(this);
+    }
+
+    handleChange = event => {
+        if (event.target.name === "nometalhao") {
+            this.setState({ nomeTalhao: event.target.value });
+        }
+        if (event.target.name === "areatalhao") {
+            this.setState({ areaTalhao: event.target.value });
+        }
     }
 
     onAddBtnClick(event) {
-        const inputList = this.state.inputList;
-        this.setState({
-            inputList: inputList.concat(
-            <div className="group"><input key={this.state.inputList} type="text" required /><span className="highlight"></span>
-            <span className="bar"></span><label>{"Talhão "+this.state.inputList.length}</label></div>
-            )
-            });
-    }
-    
-    onRemoveBtnClick(event) {
-        const inputList = this.state.inputList;
-        this.setState({
-            inputList: inputList.slice(0, this.state.inputList.length - 1)
-        });
+        let obj = [{ talhao: this.state.nomeTalhao, area: this.state.areaTalhao }]
+        this.setState({ data: [...this.state.data, ...obj] })
     }
 
     render() {
+        const { data } = this.state;
+        const columns = [{
+            Header: 'Talhão',
+            accessor: 'talhao'
+        }, {
+            Header: 'Área Cultivada(M²)',
+            accessor: 'area',
+        }]
         return (
-            <div className='bgmodal'>
-                <div style={{ background: 'white', borderRadius: '10px', width: '60%', display: 'absolute', margin: 'auto' }}>
-                    <fieldset style={{ border: 'none', marginTop: '4rem' }}>
-                        <legend style={{ fontSize: '1.5rem', fontWeight: 'bolder' }}>Área total da fazenda</legend>
-                        {this.state.inputList}
-                        <button style={{ width:'40px', padding: '0.5rem', background: '#008CBA', textDecoration: 'none', border: 'none' }} onClick={this.onAddBtnClick}>+</button>
-                        <button style={{ width:'50px', padding: '0.5rem', background: '#008CBA', textDecoration: 'none', border: 'none' }} onClick={this.onRemoveBtnClick}>-  </button>
-                    </fieldset>
+            <div style={{background: 'rgba(0,0,0,0.3)', position: "absolute", width: '100%', height: '1200px'}}>
+                <div style={{ background: 'white', borderRadius: '5px', border: 'solid 1px black', width: '90%', display: 'absolute', margin: 'auto', marginTop: '4rem', zIndex: "200" }}>
+                    <h2>Talhões</h2>
+                    <div className='input_container half' style={{ marginRight: '1%', marginLeft: '0.5%' }}>
+                        <div className="group">
+                            <input type="text" name="nometalhao" value={this.state.firstName} onChange={this.handleChange} required />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Talhão</label>
+                        </div>
+                    </div>
+                    <div className='input_container half'>
+                        <div className="group">
+                            <input type="text" name="areatalhao" value={this.state.lastName} onChange={this.handleChange} required />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Área cultivada</label>
+                        </div>
+                    </div>
+                    <button className="btncad" onClick={this.onAddBtnClick}>Add</button>
+                    <div>
+                        <ReactTable data={data} columns={columns}
+                            defaultPageSize={10}
+                            className="-striped -highlight"
+                        />
+                    </div>
                 </div>
             </div>
         );
